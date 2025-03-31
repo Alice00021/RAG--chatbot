ec10fa688a8f031e746084dbd11a962a83401b14
Alice00021 <alisa_barysheva03@mail.ru>
2025-03-31 15:26:20 +0300

    added logging for future final project in echo bot

--- echo_bot.py
+++ echo_bot.py
@@ -4,23 +4,48 @@ import sys
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
+from aiogram.exceptions import TelegramAPIError
 
+console_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler('bot.log')
+logging.basicConfig(
+    level = logging.INFO,
+    handlers=[console_handler, file_handler],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 load_dotenv()
 
 API_TOKEN = os.getenv('API_KEY')
 if not API_TOKEN:
+    logger.error("API_TOKEN не найден в переменных окружения. Проверьте файл .env")
     raise ValueError("API_TOKEN не найден в переменных окружения. Проверьте файл .env")
 
 dp = Dispatcher()
 
 @dp.message()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    try:
+        await message.answer(message.text)
+        logger.info(f"Ответил пользователю {message.from_user.id}: {message.text}")
+    except TelegramAPIError as e:
+            logger.error(f"Ошибка Telegram API: {e}")
+            await message.answer("Произошла ошибка при отправке сообщения.")
+    except Exception as e:
+        logger.error(f"Неизвестная ошибка: {e}")
+        await message.answer("Что-то не так")
 
 async def main() -> None:
     bot = Bot (token= API_TOKEN)
-    await dp.start_polling(bot)
-
+    dp.bot = bot
+    try:
+        logger.info("Запуск бота")
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.error(f"Ошибка polling: {e}")
+    finally:
+        logger.info("Остановка бота...")
+        await bot.session.close()
+         
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
