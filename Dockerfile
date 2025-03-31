77ccc70b1c5267f91bb8b0a5f3dfa9395a29560b
Alice00021 <alisa_barysheva03@mail.ru>
2025-03-31 18:39:23 +0300

    Added new bot to the chatgpt-bot branch based on OpenRouter API key. The key was taken from OpenRouter. Key deepseek-R1

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 COPY requirements.txt .
 COPY . .
 RUN pip install --no-cache-dir -r requirements.txt
-CMD ["python", "echo_bot.py"]
+CMD ["python", "chatgpt_bot.py"]
 
--- chatgpt_bot.py
+++ chatgpt_bot.py
@@ -5,6 +5,8 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.exceptions import TelegramAPIError
+import openai
+from openai import AsyncOpenAI, OpenAI
 
 console_handler = logging.StreamHandler(sys.stdout)
 file_handler = logging.FileHandler('bot.log')
@@ -17,16 +19,30 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 API_TOKEN = os.getenv('API_KEY')
-if not API_TOKEN:
-    logger.error("API_TOKEN не найден в переменных окружения. Проверьте файл .env")
-    raise ValueError("API_TOKEN не найден в переменных окружения. Проверьте файл .env")
+OPENAI_TOKEN = os.getenv('OPENAI_API_KEY')
 
+if not API_TOKEN or  not OPENAI_TOKEN:
+    logger.error("API_TOKEN или OPENAI_TOKEN не найден в переменных окружения. Проверьте файл .env")
+    raise ValueError("API_TOKEN или OPENAI_TOKEN не найден в переменных окружения. Проверьте файл .env")
+
+client = AsyncOpenAI( base_url="https://openrouter.ai/api/v1", api_key = OPENAI_TOKEN)
+""" openai.api_key = OPENAI_TOKEN """
 dp = Dispatcher()
 
 @dp.message()
-async def echo(message: types.Message):
+async def chatgpt_message(message: types.Message):
     try:
-        await message.answer(message.text)
+        completion = await client.chat.completions.create(
+            
+        model="deepseek/deepseek-r1:free",
+        messages=[{
+      "role": "user",
+      "content": f"{message}" 
+        }]
+)       
+        response = completion.choices[0].message.content
+        print(response)
+        await message.answer(response)
         logger.info(f"Ответил пользователю {message.from_user.id}: {message.text}")
     except TelegramAPIError as e:
             logger.error(f"Ошибка Telegram API: {e}")
@@ -44,7 +60,7 @@ async def main() -> None:
     except Exception as e:
         logger.error(f"Ошибка polling: {e}")
     finally:
-        logger.info("Остановка бота...")
+        logger.info("Остановка бота")
         await bot.session.close()
          
 if __name__ == '__main__':
