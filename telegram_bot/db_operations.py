395614f40a4bd408cf4158db36b5da7e57938540
Alice00021 <alisa_barysheva03@mail.ru>
2025-04-22 15:21:19 +0300

    add update_chat_status

--- telegram_bot/db_operations.py
+++ telegram_bot/db_operations.py
@@ -32,12 +32,12 @@ async def add_user(pool, telegram_id: int, username: str, first_name:str, last_n
             INSERT INTO "User"(telegram_id, username, first_name, last_name) VALUES($1, $2, $3, $4 )
         ''', telegram_id, username, first_name, last_name)  
 
-async def add_chat(pool, telegram_id:int):
+async def add_chat(pool, user_id: int) -> int:
     async with pool.acquire() as conn:
-        chat = await conn.fetchrow('SELECT chat_id FROM "Chat" WHERE user_id = $1', telegram_id)
+        chat = await conn.fetchrow('SELECT chat_id FROM "Chat" WHERE user_id = $1', user_id)
         if not chat:
-            await conn.execute('INSERT INTO "Chat" (user_id) VALUES ($1)', telegram_id)
-        chat = await conn.fetchrow('SELECT chat_id FROM "Chat" WHERE user_id = $1', telegram_id)
+            await conn.execute('INSERT INTO "Chat" (user_id) VALUES ($1)', user_id)
+            chat = await conn.fetchrow('SELECT chat_id FROM "Chat" WHERE user_id = $1', user_id)
         return chat['chat_id']
 
 async def add_message(pool, chat_id: int, sender_type: str, text: str, responder_id: int = None):
@@ -46,6 +46,14 @@ async def add_message(pool, chat_id: int, sender_type: str, text: str, responder
             'INSERT INTO "Message" (chat_id, sender_type, responder_id, text) VALUES ($1, $2, $3, $4)',
             chat_id, sender_type, responder_id, text
         )
+
+async def update_chat_status(pool, chat_id: int, status:str):
+    async with pool.acquire() as conn:
+        await conn.execute(
+            'UPDATE "Chat" SET status = $2 WHERE chat_id = $1',
+            chat_id, status
+        )
+        
 async def main():
     pool = await init_db()
     await init_db(pool)
